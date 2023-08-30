64a05594e9ba0e8d9566452fe2ed6003ea6bfc19
Adrian Blanco <ablancoc@ucenfotec.ac.cr>
2023-08-29 23:38:05 -0400

    Commit inicial con la creacion de la base del proyecto

---
+++ Nutricense/DTOs/Alimento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs
+{
+    public class Alimento : BaseDTO
+    {
+        public string Nombre { get; set; }
+        public string FuenteFibra { get; set; }
+        public string GrupoAlimenticio { get; set; }
+        public string TamannoPorcion { get; set; }
+
+    }
+}
