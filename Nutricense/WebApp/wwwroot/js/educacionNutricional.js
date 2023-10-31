31987ea3bb777292b2f95b15c1471cf8cb077b9c
Adrian Blanco <ablancoc@protonmail.com>
2023-10-31 15:08:27 -0400

    Implementacion de los articulos de educacion nutricional en forma de imagen

--- Nutricense/WebApp/wwwroot/js/educacionNutricional.js
+++ Nutricense/WebApp/wwwroot/js/educacionNutricional.js
@@ -6,7 +6,20 @@ const grupoAlimentosBtn = document.querySelector("#grupoAlimentosBtn");
 const beneficiosBtn = document.querySelector("#beneficiosBtn");
 const recomendacionesBtn = document.querySelector("#recomendacionesBtn");
 
+
+const imgArticuloAlimentos = document.querySelector("#imgArticuloAlimentos");
+const imgArticuloEjercicio = document.querySelector("#imgArticuloEjercicio");
+const imgArticuloFibra = document.querySelector("#imgArticuloFibra");
+
+
 grupoAlimentosBtn.addEventListener('click', function () {
+
+    grupoAlimentosMdl.style.display = 'block';
+
+
+})
+
+imgArticuloAlimentos.addEventListener('click', function () {
 
     grupoAlimentosMdl.style.display = 'block';
 
@@ -21,6 +34,13 @@ beneficiosBtn.addEventListener('click', function () {
 
 })
 
+imgArticuloEjercicio.addEventListener('click', function () {
+
+    beneficiosMdl.style.display = 'block';
+
+
+})
+
 
 recomendacionesBtn.addEventListener('click', function () {
 
@@ -28,6 +48,16 @@ recomendacionesBtn.addEventListener('click', function () {
 
 
 })
+
+imgArticuloFibra.addEventListener('click', function () {
+
+    recomendacionesMdl.style.display = 'block';
+
+
+})
+
+
+
 
 
 window.onclick = function (event) {
@@ -50,3 +80,4 @@ btnCerrar.forEach(btn => {
     }
 
 })
+
